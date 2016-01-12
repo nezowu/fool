451dce8974e394d76ca03a1d077be9f78c9238a9
olecya <gxendz@mail.ru>
2016-01-12 09:14:07 +0300

    12

--- card/card.sh
+++ card/card.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+IP=$1
 ##Запишим в переменные ряды карт для одной масти. Через 16 цифр ряд повторяется для следующей масти
 black_card='\U1F0A0'
 number_min=$(bc<<<"ibase=16;1F0A1")
@@ -8,6 +9,7 @@ number_min2=$(bc<<<"ibase=16;1F0AD")
 #number_min="$(printf '%b' '\U1F0A1')"
 number_max1=$(bc<<<"ibase=16;1F0AB")
 number_max2=$(bc<<<"ibase=16;1F0AE")
+
 ##Запишим набор одной масти в асоциативный массив
 z=1
 declare -A monst
@@ -19,7 +21,6 @@ done <<<"$number_min
 $(seq $number_min1 $number_max1)
 $(seq $number_min2 $number_max2)" 
 unset monst["num$z"]
-#echo ${!monst[@]}
 
 ##Создадим полный набор одной колоды и запишем в массив
 declare -a arrvar
@@ -31,12 +32,7 @@ for ((i=0; i<4; i++)); do
 		((monst[$y]+=16))
 	done
 done
-##выведем на экран все карты как есть в колоде
-#echo ${arrvar[@]}
-#echo ${#arrvar[@]}
-#for pof in ${arrvar[@]}; do
-#	echo -e "\U$(bc<<<"obase=16;$pof")"
-#done
+
 ##Перемешаем колоду
 declare -a shuf_card
 s=0
@@ -44,26 +40,19 @@ for p in $(shuf -i 0-35); do
 	shuf_card[p]=${arrvar[@]:s:1}
 	((s+=1))
 done
-#echo ${shuf_card[@]}
-#echo ${arrvar[@]:1:1}
 printColor(){
-	for tin in ${!1}; do
+	for tin in $@; do
 		if [[ $tin -gt 127150 && $tin -lt 127185 ]]; then
 			tput setaf 1
 			printf '%b ' "\U$(bc<<<"obase=16;$tin")"
 			tput sgr0
-			[[ -n $2 ]] && break
 		else
 			printf '%b ' "\U$(bc<<<"obase=16;$tin")"
-			[[ -n $2 ]] && break
 		fi
 	done
-#echo
 }
-#printColor "shuf_card[@]"
-#echo
-##раздаем карты в 3 поля(массива) но предусмотренно еще два "бой" и "полебоя"
-#declare -a shuf_card
+
+##раздаем карты в 2 поля(массива) но предусмотренно еще два "бой" и "полебоя"
 declare -a onestaf
 declare -a twostaf
 declare -a battlestaf
@@ -86,31 +75,60 @@ razdacha(){
 unset i
 }
 razdacha
-#printColor "onestaf[@]"
-#echo
-printColor "twostaf[@]"
-echo
-printColor "shuf_card[@]"
-echo
-sleep 3
 tput clear
 printHead(){
+	battlestaf=(127150 127137)
+	trashstaf=(1 5)
 	tput civis
 	#tput cnorm
 	tput cup 2 1
 	tput el
-	printColor "shuf_card[@]" 1
+	printColor ${shuf_card[@]:0:1}
 	if [[ ${#shuf_card[@]} -gt 1 ]]; then
 		tput setaf 4
 		printf '%b' "$black_card"
 		tput sgr0
 	fi
-	tput cup 6 1
+	tput cup 6 48
 	tput el
-	printColor "onestaf[@]"
+	if [[ ${#trashstaf} -gt 0 ]]; then
+		tput setaf 4
+		printf '%b' "$black_card"
+		tput sgr0
+	tput cup 10 25
+	tput el
+	if [[ ${#battlestaf[@]} -gt 0 ]]; then
+		printColor ${battlestaf[@]}
+	fi
+
+	fi
+	tput cup 14 1
+	tput el
+	if [[ -z $IP ]]; then
+		printColor ${onestaf[@]}
+		tput cup 16 40
+		tput el
+		for ((i=0; i<${#twostaf[@]}; i++)); do
+			tput setaf 4
+			printf '%b ' "$black_card"
+		done
+		tput sgr0
+	else
+		for ((i=0; i<${#onestaf[@]}; i++)); do
+			tput setaf 4
+			printf '%b ' "$black_card"
+		done
+		tput sgr0
+		tput cup 16 40
+		tput el1
+		printColor ${twostaf[@]}
+	fi
+
 }
 printHead
 sleep 2
 tput cnorm
 echo
 exit
+#array+=(six) добавить элемент в конец массива
+#${intArray[@]:(-1)}
