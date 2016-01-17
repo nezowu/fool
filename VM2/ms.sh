4dd4d65b7665bd3ecc067afd01b3b07a1f8c27ab
olecya <gxendz@mail.ru>
2016-01-17 18:29:59 +0300

    17_01_home

---
+++ VM2/ms.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+trap 'break' 2
+echo -en "\e[?9h"
+tput civis
+stty -icanon
+for ((i=0; i<100; i++)); do
+	exec 6>&1
+	exec >/dev/null
+	read -rsn 6 x
+	#tput clear
+	#string=$(od -b <<<$x)
+	#mouse=$(expr $string : '.*\(1b 5b 4d .. .. ..\)')
+	string=$(echo -n $x | hexdump -C | sed -n 's/.*1b 5b 4d.\(..\) \(..\) \(..\).*/\1\2\3/p')
+	exec 1>&6 6>&-
+	echo "$string" | tee -a textik.txt
+	unset string
+done
+echo -en "\e[?9l"
+tput cvvis
+stty icanon
